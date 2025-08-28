3d59001090304ede32d06cf2be928ed1a63f248f
DrinnDizon <aldrinpayabyab8@gmail.com>
2025-08-28 08:43:00 +0800

    Add files via upload

---
+++ calculator.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>simple calculator</title>
+</head>
+<body>
+    <form action="compute.php" method="post">
+        <label for="">Enter first Number:</label> <br>
+        <input type="number" name="num1" id=""> <br>
+        <label for="">Enter Second Number:</label> <br>
+        <input type="number" name="num2" id=""> <br>
+        <label for="">Enter third Number:</label> <br>
+        <input type="number" name="num3" id=""> <br>
+        <label for="">Enter fourth Number:</label> <br>
+        <input type="number" name="num4" id=""> <br> <br>
+        <input type="submit" value="Compute">
+    </form>
+</body>
+</html>
